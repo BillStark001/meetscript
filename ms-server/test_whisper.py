ee6d9e9cecd78a808654a63d6c0f3bfe840c787e
Bill Stark <billstark001@gmail.com>
2023-10-01 05:30:04 +0900

    update test suite

--- ms-server/test_whisper.py
+++ ms-server/test_whisper.py
@@ -1,4 +1,7 @@
 import asyncio
+import os
+import time
+import torch
 
 from subprocess import CalledProcessError, run
 
@@ -27,7 +30,7 @@ def load_audio(file: str, sr: int = 16000):
   # return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
 
 async def test():
-  audio = load_audio('/Users/billstark001/Desktop/micro-machines.wav')
+  audio = load_audio(os.path.expanduser('~/micro-machines.wav'))
   L = 1000
   S = []
   last_time = 0
@@ -40,6 +43,9 @@ async def test():
   worker = WhisperWorker('small')
   await worker.init_model()
   
+  print(f'CUDA is available: {torch.cuda.is_available()}')
+  
+  tstart_all = time.time()
   for i in range(len(S) // 4 + 1):
     if not S:
       break
@@ -47,9 +53,13 @@ async def test():
     S = S[4:] if len(S) > 4 else []
     for t, s in Sh:
       await worker.enqueue_chunk(t, s)
+    tstart = time.time()
     res = await worker.transcribe_once()
+    tend = time.time() - tstart
     for result in res:
-      print(result.text if not result.partial else f'> [{result.start}-{result.end}] {result.text}')
+      print(f'{" " if not result.partial else ">"} [{tend:.3f}s] [{result.start}-{result.end}]', result.text)
+  tend_all = time.time() - tstart_all
+  print(f'Elapsed time: {tend_all:.3f}s')
   
 if __name__ == '__main__':
   asyncio.run(test())
